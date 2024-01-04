c1619bec9b11fb12ae35d3e0ffa9974a16b9eaf1
louie <louiecasula@gmail.com>
2024-01-03 21:01:07 -0500

    Typo...

--- src/test/java/com/example/demo/YeechatApplicationTests.java
+++ src/test/java/com/example/demo/YeechatApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ZeechatApplicationTests {
+class YeechatApplicationTests {
 
 	@Test
 	void contextLoads() {
