0b6a9c97bb73ea2d1ffaecc51a233a900d173a68
louie <louiecasula@gmail.com>
2024-01-08 11:50:03 -0500

    Updated add, remove, and get all users methods in WorkspaceService.

--- src/main/java/com/example/demo/services/WorkspaceService.java
+++ src/main/java/com/example/demo/services/WorkspaceService.java
@@ -3,20 +3,24 @@ package com.example.demo.services;
 import com.example.demo.dtos.WorkspaceDto;
 import com.example.demo.models.User;
 import com.example.demo.models.Workspace;
+import com.example.demo.repositories.UserRepository;
 import com.example.demo.repositories.WorkspaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class WorkspaceService {
 
     private final WorkspaceRepository workspaceRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public WorkspaceService(WorkspaceRepository workspaceRepository) {
+    public WorkspaceService(WorkspaceRepository workspaceRepository, UserRepository userRepository) {
         this.workspaceRepository = workspaceRepository;
+        this.userRepository = userRepository;
     }
 
     public Workspace createWorkspace(WorkspaceDto workspaceDto) {
@@ -51,15 +55,31 @@ public class WorkspaceService {
         workspaceRepository.delete(workspace);
     }
 
-    public User addUserToWorkspace(Long workspaceId, Long userId) {
-        return null;
+    public User addUserToWorkspace(Long workspaceId, Long userId) throws Exception {
+        Workspace workspace = workspaceRepository.findById(workspaceId)
+                .orElseThrow(() -> new Exception("Workspace doesn't exist"));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new Exception("User doesn't exist"));
+        workspace.getUsers().add(user);
+        user.getWorkspaces().add(workspace);
+        workspaceRepository.save(workspace);
+        return userRepository.save(user);
     }
 
-    public void removeUserFromWorkspace(Long workspaceId, Long userId) {
-
+    public void removeUserFromWorkspace(Long workspaceId, Long userId) throws Exception {
+        Workspace workspace = workspaceRepository.findById(workspaceId)
+                .orElseThrow(() -> new Exception("Workspace doesn't exist"));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new Exception("User doesn't exist"));
+        workspace.getUsers().remove(user);
+        user.getWorkspaces().remove(workspace);
+        workspaceRepository.save(workspace);
+        userRepository.save(user);
     }
 
-    public List<User> getAllUsersInWorkspace(Long workspaceId) {
-        return null;
+    public Set<User> getAllUsersInWorkspace(Long workspaceId) throws Exception {
+        Workspace workspace = workspaceRepository.findById(workspaceId)
+                .orElseThrow(() -> new Exception("Workspace doesn't exist"));
+        return workspace.getUsers();
     }
 }
