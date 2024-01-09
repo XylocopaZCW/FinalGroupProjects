03b808e5e557b4ddaa00d93df6ecbee4c5baab3d
NathanG741 <nathan.guardado@yahoo.com>
2024-01-08 19:26:23 -0500

    good evening YeeChatters pt3

--- src/main/java/com/example/demo/services/MessageService.java
+++ src/main/java/com/example/demo/services/MessageService.java
@@ -1,10 +1,14 @@
 package com.example.demo.services;
 
 import com.example.demo.dtos.MessageDto;
+import com.example.demo.dtos.WorkspaceDto;
 import com.example.demo.models.Message;
+import com.example.demo.models.Workspace;
 import com.example.demo.repositories.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MessageService {
@@ -24,14 +28,25 @@ public class MessageService {
         return messageRepository.save(message);
     }
 
+    public Message getMessageById(Long messageId) throws Exception {
+        return messageRepository.findById(messageId)
+                .orElseThrow(() -> new Exception("Message doesn't exist"));
+    }
+
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    public Message editMessage(Long messageId, MessageDto messageDto) throws Exception {
+        Message message = messageRepository.findById(messageId)
+                .orElseThrow(() -> new Exception("Message doesn't exist"));
+        message.setBody(messageDto.getBody());
+        return messageRepository.save(message);
+    }
+
     public void deleteMessage(MessageDto messageDto) {
         Message message = messageRepository.getReferenceById(messageDto.getMessageId());
         messageRepository.delete(message);
     }
 
-    // getMessageById(Long messageId)
-
-    // editMessage(Long messageId, MessageDto messageDto)
-
-    // listMessagesInChannel(Long channelId)
 }
