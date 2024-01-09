7e3e8aeaf147e92a2162157079711f759b633e7b
NathanG741 <nathan.guardado@yahoo.com>
2024-01-08 19:37:38 -0500

    good evening YeeChatters pt3.5

--- src/main/java/com/example/demo/controllers/MessageController.java
+++ src/main/java/com/example/demo/controllers/MessageController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/messages")
@@ -24,6 +25,36 @@ public class MessageController {
         try {
             Message newMessage = messageService.sendMessage(messageDto);
             return new ResponseEntity<>(newMessage, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+     @GetMapping("/getMessageById")
+     public ResponseEntity<?> getMessageById(@RequestBody MessageDto messageDto) {
+        try {
+            Message message = messageService.getMessageById(messageDto.getMessageId());
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/editMessage")
+    public ResponseEntity<?> editMessage(@RequestBody MessageDto messageDto){
+        try{
+            messageService.editMessage(messageDto.getMessageId(), messageDto);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/getAllMessages")
+    public ResponseEntity<?> getAllMessages(@RequestBody MessageDto messageDto) {
+        try {
+            List<Message> messageList =  messageService.getAllMessages();
+            return new ResponseEntity<>(messageList, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
--- src/main/java/com/example/demo/services/MessageService.java
+++ src/main/java/com/example/demo/services/MessageService.java
@@ -1,13 +1,10 @@
 package com.example.demo.services;
 
 import com.example.demo.dtos.MessageDto;
-import com.example.demo.dtos.WorkspaceDto;
 import com.example.demo.models.Message;
-import com.example.demo.models.Workspace;
 import com.example.demo.repositories.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
