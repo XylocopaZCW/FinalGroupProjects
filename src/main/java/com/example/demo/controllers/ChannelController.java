8df1a21276bc25e8661bf5ae389ccbd18381a54a
louie <louiecasula@gmail.com>
2024-01-10 12:52:51 -0500

    Added @CrossOrigin to each controller.

--- src/main/java/com/example/demo/controllers/ChannelController.java
+++ src/main/java/com/example/demo/controllers/ChannelController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/channels")
 public class ChannelController {
     private final ChannelService channelService;
